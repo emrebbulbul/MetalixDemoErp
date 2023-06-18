98489daf34461e3f3ce934289b01aed0c2009e99
emrebulbul <emrebbulbul@gmail.com>
2023-06-18 16:16:28 +0300

    Dbset added.

--- MetalixDemoErp/DataAccessLayer/Context.cs
+++ MetalixDemoErp/DataAccessLayer/Context.cs
@@ -1,4 +1,5 @@
 ﻿
+using MetalixDemoErp.Entitiy;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,6 +11,9 @@ namespace MetalixDemoErp.DataAccessLayer
             {
                 optionsBuilder.UseSqlServer("server=DESKTOP-ICTBULH\\SQLEXPRESS;database=MetalixDb; integrated security=true;TrustServerCertificate=True;Trusted_Connection=True", options => options.EnableRetryOnFailure());
             }
-        
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<DangilNo> DangilNoes { get; set; }
+
+
     }
 }
