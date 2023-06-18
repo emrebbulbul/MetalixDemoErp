75ce3981cc015f5b9bc802d2534fd24c9950cf10
emrebulbul <emrebbulbul@gmail.com>
2023-06-18 16:11:40 +0300

    Entitiy added.

---
+++ MetalixDemoErp/Entitiy/DangilNo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MetalixDemoErp.Entitiy
+{
+    public class DangilNo
+    {
+        [Key]
+        public int DangilId { get; set; }
+        public int Dangilno { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
