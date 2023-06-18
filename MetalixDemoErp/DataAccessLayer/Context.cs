14b821996432f70425db133fd04f1a7b2f74843b
eminbulbul <113800032+eminbbulbul@users.noreply.github.com>
2023-06-18 16:11:57 +0300

    database

---
+++ MetalixDemoErp/DataAccessLayer/Context.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetalixDemoErp.DataAccessLayer
+{
+    public class Context : DbContext
+    {
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer("server=DESKTOP-ICTBULH\\SQLEXPRESS;database=MetalixDb; integrated security=true;TrustServerCertificate=True;Trusted_Connection=True", options => options.EnableRetryOnFailure());
+            }
+        
+    }
+}
